89a56c022719e45980ef5cb830db0f17ea10e304
lindraaa <98926528+lindraaa@users.noreply.github.com>
2023-09-08 15:37:10 +0800

    Create withuserinput.js

---
+++ withuserinput.js
@@ -0,0 +1,14 @@
+function reverseString(inputString){
+    let n_str = "";
+
+    for(let i = inputString.length -1; i>=0; --i){
+        n_str += inputString[i];        
+    }
+    console.log("The word in reverse : "+ n_str);
+
+}
+
+
+
+const inputString = (prompt("Enter a number: "));
+reverseString(inputString);
