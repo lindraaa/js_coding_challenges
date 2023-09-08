48d42506647de60026390200ffab97a2d27d4bdf
lindraaa <98926528+lindraaa@users.noreply.github.com>
2023-09-08 14:12:07 +0800

    Create final.js

---
+++ final.js
@@ -0,0 +1,28 @@
+//Without using if else statement
+function checkEvenOrOdd(num){
+    const result  = (num % 2 ==0)? "Even" : "Odd";
+    return result;
+    
+                     
+};
+function PositiveorNegative(num){
+    const sign = (num >0)? "Postive" :"Negative";
+    return sign;
+}
+
+
+while(true){
+    const num = parseFloat(prompt("Enter a number: "));
+
+    if(!isNaN(num)){
+        const res = checkEvenOrOdd(num);
+        const sig = PositiveorNegative(num);
+        alert("The number is " + (sig)+" "+(res));
+    }
+    else{
+        alert("Enter Integer only")
+    }
+
+    
+
+}
